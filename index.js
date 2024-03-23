7df136625c29268eb7d3aef27e469e640f04e306
Meikan-Sankan <meikanseikan@gmail.com>
2024-03-23 13:50:50 +0300

    strings commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+console.log('currentUser is defined:', true);
+console.log('welcomeMessage contains "Welcome to Flatbook, ":', welcomeMessage.includes(`Welcome to Flatbook, `));
+console.log('welcomeMessage contains the value of the \'currentUser\' variable:', welcomeMessage.includes(currentUser));
+console.log("welcomeMessage ends with an exclamation point!:", welcomeMessage.endsWith('!'));
+console.log('excitedWelcomeMessage contains "WELCOME TO FLATBOOK, ":', excitedWelcomeMessage.includes(`WELCOME TO FLATBOOK, `));
+console.log('excitedWelcomeMessage contains the value of the \'currentUser\' variable:', excitedWelcomeMessage.includes(currentUser));
+console.log("excitedWelcomeMessage ends with an exclamation point:", excitedWelcomeMessage.endsWith('!'))
+console.log('shortGreeting contains "Welcome, ":', shortGreeting.includes(`Welcome, `));
+console.log('shortGreeting contains the first initial of the name stored in the \'currentUser\' variable:', shortGreeting.includes(currentUser.charAt(0)));
+console.log("shortGreeting ends with an exclamation point:", shortGreeting.endsWith('!'))
+;
